d61dd4604c9cd279377c66888b7588a6e1485d47
Ag Stephens <ag.stephens@stfc.ac.uk>
2020-11-19 17:03:16 +0000

    Updated start date for land data

--- python/_common.py
+++ python/_common.py
@@ -16,10 +16,13 @@ inv_stations = {
     'marine': 2
 }
 
+
+start_year = 1755
+
 year_ranges = {
-    "land": range(1755, current_year + 1),
+    "land": range(start_year, current_year + 1),
     "marine": range(1946, current_year + 1)
 }
 
-all_years = range(1761, current_year + 1)
+all_years = range(start_year, current_year + 1)
 
